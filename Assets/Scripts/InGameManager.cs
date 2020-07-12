2e76adb9ca18ebf0eb5e47c8592e4cf823dad34f
Carambula <antibog6@gmail.com>
2020-07-12 19:57:00 +0300

    add server

---
+++ Assets/Scripts/InGameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameManager : MonoBehaviourPunCallbacks {
+
+	public GameObject canvas;
+	private bool canvasActive = true;
+
+	void Start() {
+		canvasActive = !canvasActive;
+		canvas.SetActive(canvasActive);
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			canvasActive = !canvasActive;
+			canvas.SetActive(canvasActive);
+		}
+	}
+
+	public void Leave() { PhotonNetwork.LeaveRoom(); }
+
+	public override void OnLeftRoom() { SceneManager.LoadScene(0); }
+
+	public override void OnPlayerEnteredRoom(Player newPlayer) { Debug.Log($"{newPlayer.NickName} entered room"); }
+
+	public override void OnPlayerLeftRoom(Player otherPlayer) { Debug.Log($"{otherPlayer.NickName} left room"); }
+}
