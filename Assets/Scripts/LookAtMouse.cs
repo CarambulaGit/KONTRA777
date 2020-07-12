aa287c9c78abb5e4150c73e19364fe14bc0e9692
Carambula <antibog6@gmail.com>
2020-07-13 01:57:33 +0300

    add lookAtMouse.cs

---
+++ Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour {
+	void Start() { }
+
+	void Update() {
+		Vector3 aimDir = (Camera.main.WorldToScreenPoint(transform.position) - Input.mousePosition).normalized;
+		float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
+		transform.eulerAngles = new Vector3(0,0, angle + 90);
+	Debug.Log(angle);
+	}
+}
