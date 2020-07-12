1252d0214af9c6100d87431a5ec8af8ada190050
Carambula <antibog6@gmail.com>
2020-07-13 02:06:03 +0300

    fix rotation

--- Assets/Scripts/LookAtMouse.cs
+++ Assets/Scripts/LookAtMouse.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class LookAtMouse : MonoBehaviour {
-	void Start() { }
+	private PhotonView photonView;
+	void Start() { photonView = GetComponent<PhotonView>(); }
 
 	void Update() {
+		if (!photonView.IsMine) return;
 		Vector3 aimDir = (Camera.main.WorldToScreenPoint(transform.position) - Input.mousePosition).normalized;
 		float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
-		transform.eulerAngles = new Vector3(0,0, angle + 90);
-	Debug.Log(angle);
+		transform.eulerAngles = new Vector3(0, 0, angle + 90);
 	}
 }
