3cef85c499154eb014c8cf4d831c5d4007370ac5
Carambula <antibog6@gmail.com>
2020-10-25 17:47:01 +0200

    fix camera position

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public const string playerCameraTag = "PlayerCam";
+	public readonly Vector3 playerCameraOffset = new Vector3(0, 0, -10);
+	private PhotonView photonView;
+	private Transform playerCamera;
+
+	void Start() {
+		photonView = GetComponent<PhotonView>();
+		playerCamera = GameObject.FindGameObjectWithTag(playerCameraTag).transform;
+	}
+
+	void Update() {
+		if (photonView.IsMine) { playerCamera.position = transform.position + playerCameraOffset; }
+	}
+}
--- Assets/Scripts/LookAtMouse.cs
+++ Assets/Scripts/LookAtMouse.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 
 public class LookAtMouse : MonoBehaviour {
 	private PhotonView photonView;
-	void Start() { photonView = GetComponent<PhotonView>(); }
+	private Camera playerCamera;
+
+	void Start() {
+		photonView = GetComponent<PhotonView>();
+		playerCamera = GameObject.FindGameObjectWithTag(CameraController.playerCameraTag).GetComponent<Camera>();
+	}
 
 	void Update() {
 		if (!photonView.IsMine) return;
-		Vector3 aimDir = (Camera.main.WorldToScreenPoint(transform.position) - Input.mousePosition).normalized;
+		Vector3 aimDir = (playerCamera.WorldToScreenPoint(transform.position) - Input.mousePosition).normalized;
 		float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
 		transform.eulerAngles = new Vector3(0, 0, angle + 90);
 	}
