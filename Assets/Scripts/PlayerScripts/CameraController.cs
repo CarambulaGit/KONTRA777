ec87022b3f265814236f130e4585e24059c092a4
Carambula <antibog6@gmail.com>
2021-01-05 21:34:30 +0200

    little camera fix

--- Assets/Scripts/PlayerScripts/CameraController.cs
+++ Assets/Scripts/PlayerScripts/CameraController.cs
@@ -5,13 +5,14 @@ namespace PlayerScripts
 {
 	public class CameraController : MonoBehaviour {
 		public const string playerCameraTag = "PlayerCam";
-		public readonly Vector3 playerCameraOffset = new Vector3(0, 0, -10);
+		private Vector3 playerCameraOffset;
 		private PhotonView photonView;
 		private Transform playerCamera;
 
 		void Start() {
 			photonView = GetComponent<PhotonView>();
 			playerCamera = GameObject.FindGameObjectWithTag(playerCameraTag).transform;
+			playerCameraOffset = new Vector3(0, 0, playerCamera.position.z);
 		}
 
 		void Update() {
