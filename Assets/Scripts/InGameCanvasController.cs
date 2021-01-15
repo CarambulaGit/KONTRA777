5254b8f32304bd5974984483aea0a5475d8c8e27
NikitosPes <kutsokon95@gmail.com>
2021-01-15 21:43:44 +0300

    Made HealthBar

--- Assets/Scripts/InGameCanvasController.cs
+++ Assets/Scripts/InGameCanvasController.cs
@@ -4,6 +4,7 @@ using Photon.Pun;
 using PlayerScripts;
 using Resources.Classes;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InGameCanvasController : MonoBehaviour {
     public enum CanvasStatus : ushort {
@@ -16,19 +17,36 @@ public class InGameCanvasController : MonoBehaviour {
     public GameObject sGMHost;
     public GameObject sGMOthers;
     public GameObject escMenu;
+    public Slider healthBar;
+    public Gradient gradient;
+    public Image fill;
     public CanvasStatus canvasStatus;
+    private bool init;
 
     void Start() {
+        
         canvasStatus = CanvasStatus.StartGameMenu;
         OnChangedCanvasStatus();
         SetNecessaryStartGameMenu();
     }
 
     void Update() {
+        if (!init && PlayerSoldier.localPlayer != null) {
+            healthBar.maxValue = PlayerSoldier.localPlayer.soldier.health;
+            fill.color = gradient.Evaluate(1f);
+            init = true;
+        }
+
         if (Input.GetKeyDown(KeyCode.Escape)) {
             canvasStatus = CanvasStatus.EscMenu;
             OnChangedCanvasStatus();
         }
+
+        if (init) {
+            healthBar.value = PlayerSoldier.localPlayer.health;
+            fill.color = gradient.Evaluate(healthBar.normalizedValue);
+        }
+       
     }
 
     public void OnChangedCanvasStatus() {
